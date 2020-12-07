a125422b8dd703638eb975cbdb86a6a2b8fc27ce
Rigzin08 <ugyenrinzin08@gmail.com>
2020-12-07 11:53:05 +0600

    sync

--- public/js/app.js
+++ public/js/app.js
@@ -82464,8 +82464,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\office\EMIS Enhancement\emisv3-ace\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\office\EMIS Enhancement\emisv3-ace\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! D:\development\emisv3-ace\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\development\emisv3-ace\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
